9d937155529385974d539bca3870b0593624591f
zen <ashgrayblue0@gmail.com>
2024-06-16 20:48:50 +0900

    feat: NavBar 제작

---
+++ src/component/header/Navbar.jsx
@@ -0,0 +1,66 @@
+import symbol from '../../utils/imgs/symbol_black.png';
+import classNames from "classnames";
+import {useState} from "react";
+
+const Navbar = ({currentMenu}) => {
+  const [menuToggle, setMenuToggle] = useState(false);
+  const menu = ['Home', 'About Us', 'Plan', 'Travel', 'Record'];
+
+  return (
+    <nav>
+      <div className="h-16 max-w-7xl mx-auto px-3">
+
+        <div className="h-16 flex justify-between items-center">
+
+          {/* 메뉴 */}
+          <div>
+            <a href="#" className='flex items-center py-5 px-2 text-gray-700'>
+              <img src={symbol} alt="symbol" className='w-9 h-9'/>
+            </a>
+          </div>
+
+          {/* 메뉴2 */}
+          <div className='hidden md:flex space-x-3'>
+            {menu.map(v => (
+              <a href="#" className="flex items-center py-5 px-2 text-gray-700" key={v}>
+                <span
+                  className={`text-lg font-normal py-5 px-3 text-gray-700 hover:text-gray-900 ${v === currentMenu ? 'underline underline-offset-8' : ''}`}>
+                  {v}
+                </span>
+              </a>
+            ))}
+          </div>
+
+          {/* 모바일 메뉴 */}
+          <div className='md:hidden flex items-center'>
+            <button
+              onClick={() => setMenuToggle(!menuToggle)}
+            >
+              {!menuToggle ? (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2"
+                     stroke="currentColor" className="h-9 w-9">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                </svg>
+              ) : (
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="2"
+                     stroke="currentColor" className="h-9 w-9">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                </svg>
+              )}
+            </button>
+          </div>
+        </div>
+
+        {/* 모바일 메뉴 아이템 */}
+        <div className={classNames('md:hidden', {hidden: !menuToggle})}>
+          {menu.map(v => (
+            <a href="#" className="block py-2 px-4 text-sm hover:bg-gray-200" key={`mobile_${v}`}>{v}</a>
+          ))}
+        </div>
+
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/component/home/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../header/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <header>
+        <Navbar currentMenu='Home'></Navbar>
+      </header>
+
+    </div>
+  );
+}
+
+export default Home;
